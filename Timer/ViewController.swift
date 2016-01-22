0fb8b5a294bec83fbc5d699c6872ffe649e81c10
Mads Peter Emborg <mads@embo.dk>
2016-01-22 20:01:42 +0100

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Mads Peter Emborg on 22/01/2016.
+//  Copyright © 2016 Mads Peter Emborg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
