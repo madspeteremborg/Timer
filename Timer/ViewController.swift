355b354ca7128e3487c275ebb3740ab7f918ef45
Mads Peter Emborg <mads@embo.dk>
2016-01-23 11:20:47 +0100

    Tilføjet nogle UI settings

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayLabel: UITextField!
+    @IBOutlet weak var minuteSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
